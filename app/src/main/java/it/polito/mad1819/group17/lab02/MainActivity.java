fc0e46cfc0177c35cc45d907aedaba92ddcfcf65
Pdipedro25 <34885983+Pdipedro25@users.noreply.github.com>
2019-03-29 17:03:02 +0100

    Defining first framework for fragments

--- app/src/main/java/it/polito/mad1819/group17/lab02/MainActivity.java
+++ app/src/main/java/it/polito/mad1819/group17/lab02/MainActivity.java
@@ -3,6 +3,8 @@ package it.polito.mad1819.group17.lab02;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -10,6 +12,13 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    final Fragment offersFragment = new OffersFragment();
+    final Fragment ordersFragment = new OrdersFragment();
+    final Fragment profileFragment = new ProfileFragment();
+    final FragmentManager fm = getSupportFragmentManager();
+    Fragment active = offersFragment;
+
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,14 +26,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_profile:
+                    fm.beginTransaction().hide(active).show(offersFragment).commit();
+                    active = offersFragment;
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_dailyoffer:
+                    fm.beginTransaction().hide(active).show(ordersFragment).commit();
+                    active = ordersFragment;
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_orders:
+                    fm.beginTransaction().hide(active).show(profileFragment).commit();
+                    active = profileFragment;
                     return true;
             }
             return false;
@@ -35,10 +47,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fm.beginTransaction().add(R.id.main_container, profileFragment, "3").hide(profileFragment).commit();
+        fm.beginTransaction().add(R.id.main_container, ordersFragment, "2").hide(ordersFragment).commit();
+        fm.beginTransaction().add(R.id.main_container, offersFragment, "1").commit();
 //
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.navigation_orders);
     }
 
 }
