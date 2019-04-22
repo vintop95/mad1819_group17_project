eb98ca9d4fe4db63734ad094586b852f1c84380d
valeriomieuli <valeriomieuli@gmail.com>
2019-04-22 23:54:36 +0200

    integration of orders handling almost done

--- restaurateur/src/main/java/it/polito/mad1819/group17/deliveryapp/restaurateur/orders/Order.java
+++ restaurateur/src/main/java/it/polito/mad1819/group17/deliveryapp/restaurateur/orders/Order.java
@@ -1,24 +1,104 @@
 package it.polito.mad1819.group17.deliveryapp.restaurateur.orders;
 
 import java.io.Serializable;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 
-public class Order implements Serializable {
+public class Order implements Serializable/*, Parcelable*/ {
 
     public final static String STATE1 = "Accepted";
     public final static String STATE2 = "In preparation";
     public final static String STATE3 = "Delivering";
 
-    private int number;
+    private String id;
+    //private String restaurant_id;
     private String customer_name;
     private String customer_phone;
-    private String delivery_date;
-    private String delivery_time;
+    private String delivery_timestamp;
     private String delivery_address;
+    private HashMap<String, String> state_stateTime;
+    private HashMap<String, Integer> item_itemQuantity;
     private String notes;
+    private String rider_id;
+    private String rider_name;
+    private String rider_phone;
+    private String sorting_field;
+    private String notified = "false";
+
+
+    public Order() {
+
+    }
+
+    public Order(String id, /*String restaurant_id, */String customer_name, String customer_phone, String delivery_timestamp, String delivery_address, HashMap<String, String> state_stateTime, HashMap<String, Integer> item_itemQuantity, String notes, String rider_id, String rider_name, String rider_phone, String sorting_field, String notified) {
+        this.id = id;
+        //this.restaurant_id = restaurant_id;
+        this.customer_name = customer_name;
+        this.customer_phone = customer_phone;
+        this.delivery_timestamp = delivery_timestamp;
+        this.delivery_address = delivery_address;
+        this.state_stateTime = state_stateTime;
+        this.item_itemQuantity = item_itemQuantity;
+        this.notes = notes;
+        this.rider_id = rider_id;
+        this.rider_name = rider_name;
+        this.rider_phone = rider_phone;
+        this.sorting_field = sorting_field;
+        this.notified = notified;
+    }
+
+    public String getRider_id() {
+        return rider_id;
+    }
+
+    public void setRider_id(String rider_id) {
+        this.rider_id = rider_id;
+    }
+
+    public String getRider_name() {
+        return rider_name;
+    }
+
+    public void setRider_name(String rider_name) {
+        this.rider_name = rider_name;
+    }
+
+    public String getRider_phone() {
+        return rider_phone;
+    }
+
+    public void setRider_phone(String rider_phone) {
+        this.rider_phone = rider_phone;
+    }
+
+    public String getNotified() {
+        return notified;
+    }
+
+    public void setNotified(String notified) {
+        this.notified = notified;
+    }
+
+    /*public void setRestaurant_id(String restaurant_id) {
+        this.restaurant_id = restaurant_id;
+    }*/
+
+    public String getSorting_field() {
+        return sorting_field;
+    }
+
+    public void setSorting_field(String sorting_field) {
+        this.sorting_field = sorting_field;
+    }
+
+    public void setDelivery_timestamp(String delivery_timestamp) {
+        this.delivery_timestamp = delivery_timestamp;
+    }
+
+    public void setItem_itemQuantity(HashMap<String, Integer> item_itemQuantity) {
+        this.item_itemQuantity = item_itemQuantity;
+    }
 
     public String getDelivery_address() {
         return delivery_address;
@@ -28,6 +108,18 @@ public class Order implements Serializable {
         this.delivery_address = delivery_address;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /*public String getRestaurant_id() {
+        return restaurant_id;
+    }*/
+
     public String getNotes() {
         return notes;
     }
@@ -36,37 +128,16 @@ public class Order implements Serializable {
         this.notes = notes;
     }
 
-    private HashMap<String, String> state_stateTime;
-    private HashMap<String, Integer> item_itemQuantity;
-
-    private Date delivery_timestamp;
-
-
-    public Order(int number, String customer_name, String customer_phone, String delivery_time, String delivery_date, String delivery_address, HashMap<String, String> state_stateTime, HashMap<String, Integer> item_itemQuantity, String notes) {
-        this.number = number;
-        this.customer_name = customer_name;
-        this.customer_phone = customer_phone;
-        this.delivery_time = delivery_time;
-        this.delivery_date = delivery_date;
-        this.delivery_address = delivery_address;
-        this.state_stateTime = state_stateTime;
-        this.item_itemQuantity = item_itemQuantity;
-        this.notes = notes;
-
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-        try {
-            this.delivery_timestamp = simpleDateFormat.parse(delivery_date + " " + delivery_time);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void setItem_itemQuantity(HashMap<String, Integer> item_itemQuantity) {
-        this.item_itemQuantity = item_itemQuantity;
-    }
-
-    public Date getDelivery_timestamp() {
+    public String getDelivery_timestamp() {
         return delivery_timestamp;
+    }
+
+    public String getDelivery_date() {
+        return this.delivery_timestamp.split(" ")[0];
+    }
+
+    public String getDelivery_time() {
+        return this.delivery_timestamp.split(" ")[1];
     }
 
     public HashMap<String, String> getState_stateTime() {
@@ -81,12 +152,6 @@ public class Order implements Serializable {
         return item_itemQuantity;
     }
 
-    /*
-    public void setItem_itemQuantity(HashMap<String, Integer> item_itemQuantity) {
-        this.item_itemQuantity = item_itemQuantity;
-    }*/
-
-
     public String getCustomer_name() {
         return customer_name;
     }
@@ -95,55 +160,9 @@ public class Order implements Serializable {
         this.customer_name = customer_name;
     }
 
-    public String getDelivery_time() {
-        return delivery_time;
-    }
-
-    public void setDelivery_time(String delivery_time) {
-        this.delivery_time = delivery_time;
-    }
-
-    public String getDelivery_date() {
-        return delivery_date;
-    }
-
-    public void setDelivery_date(String delivery_date) {
-        this.delivery_date = delivery_date;
-    }
-
-    /*public HashMap<String, Integer> getOrderedItems_orderedQty() {
-        return orderedItems_orderedQty;
-    }
-
-    public void setOrderedItems_orderedQty(HashMap<String, Integer> orderedItems_orderedQty) {
-        this.orderedItems_orderedQty = orderedItems_orderedQty;
-    }
-
-    public HashMap<String, String> getState_stateTime() {
-        return state_stateTime;
-    }
-
-    public void setState_stateTime(HashMap<String, String> state_stateTime) {
-        this.state_stateTime = state_stateTime;
-    }*/
-
-    public Order(int number, String user_phone) {
-        this.number = number;
-        this.customer_phone = user_phone;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
     public String getCustomer_phone() {
         return customer_phone;
     }
-
 
     public void setCustomer_phone(String customer_phone) {
         this.customer_phone = customer_phone;
@@ -157,9 +176,9 @@ public class Order implements Serializable {
     }
 
     public String getCurrentState() {
-        if (state_stateTime.get(STATE3) != null)
+        if (state_stateTime.get("state3") != null)
             return STATE3;
-        else if (state_stateTime.get(STATE2) != null)
+        else if (state_stateTime.get("state2") != null)
             return STATE2;
         else
             return STATE1;
@@ -169,19 +188,19 @@ public class Order implements Serializable {
         String state_history = "";
         switch (getCurrentState()) {
             case STATE1:
-                state_history = getState_stateTime().get(Order.STATE1) + " " + Order.STATE1;
-                state_history += System.lineSeparator() + "--:--" + " " + Order.STATE2;
-                state_history += System.lineSeparator() + "--:--" + " " + Order.STATE3;
+                state_history = getState_stateTime().get("state1") + " " + Order.STATE1;
+                state_history += "\n" + "----/--/-- --:--" + " " + Order.STATE2;
+                state_history += "\n" + "----/--/-- --:--" + " " + Order.STATE3;
                 break;
             case STATE2:
-                state_history = getState_stateTime().get(Order.STATE1) + "  " + Order.STATE1;
-                state_history += System.lineSeparator() + getState_stateTime().get(Order.STATE2) + "  " + Order.STATE2;
-                state_history += System.lineSeparator() + "--:--" + "  " + Order.STATE3;
+                state_history = getState_stateTime().get("state1") + "  " + Order.STATE1;
+                state_history += "\n" + getState_stateTime().get("state2") + "  " + Order.STATE2;
+                state_history += "\n" + "----/--/-- --:--" + "  " + Order.STATE3;
                 break;
             case STATE3:
-                state_history = getState_stateTime().get(Order.STATE1) + "  " + Order.STATE1;
-                state_history += System.lineSeparator() + getState_stateTime().get(Order.STATE2) + "  " + Order.STATE2;
-                state_history += System.lineSeparator() + getState_stateTime().get(Order.STATE3) + "  " + Order.STATE3;
+                state_history = getState_stateTime().get("state1") + "  " + Order.STATE1;
+                state_history += "\n" + getState_stateTime().get("state2") + "  " + Order.STATE2;
+                state_history += "\n" + getState_stateTime().get("state3") + "  " + Order.STATE3;
                 break;
         }
         return state_history;
@@ -191,13 +210,18 @@ public class Order implements Serializable {
         if (getCurrentState() == STATE3)
             return false;
         else {
-            SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
-            String currentTime = formatter.format(new Date());
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+            String currentTimestamp = formatter.format(new Date());
 
-            if (getCurrentState() == STATE1)
-                state_stateTime.put(STATE2, currentTime);
-            else if (getCurrentState() == STATE2)
-                state_stateTime.put(STATE3, currentTime);
+            if (getCurrentState() == STATE1) {
+                this.state_stateTime.put("state2", currentTimestamp);
+                //this.sorting_field = this.sorting_field.split("_")[0] + "_state0_" + this.sorting_field.split("_")[2];
+
+            } else if (getCurrentState() == STATE2) {
+                this.state_stateTime.put("state3", currentTimestamp);
+                //this.sorting_field = this.sorting_field.split("_")[0] + "_state3_" + this.sorting_field.split("_")[2];
+                this.sorting_field = "state3_" + this.sorting_field.split("_")[1];
+            }
             return true;
         }
     }
--- restaurateur/src/main/java/it/polito/mad1819/group17/deliveryapp/restaurateur/orders/OrderDetailsActivity.java
+++ restaurateur/src/main/java/it/polito/mad1819/group17/deliveryapp/restaurateur/orders/OrderDetailsActivity.java
@@ -3,18 +3,25 @@ package it.polito.mad1819.group17.deliveryapp.restaurateur.orders;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.text.Html;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.support.v7.widget.Toolbar;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.time.LocalDate;
 
 import it.polito.mad1819.group17.restaurateur.R;
 
@@ -33,9 +40,11 @@ public class OrderDetailsActivity extends AppCompatActivity {
     private TextView txt_delivery_address;
     private TextView txt_order_notes;
     private Button btn_next_state;
+    private CardView card_rider;
+    private TextView txt_rider_name;
+    private TextView txt_rider_phone;
 
-    private ArrayList<Order> inputOrders;
-    private int inputPosition;
+    private Order inputOrder;
 
     private void showBackArrowOnToolbar() {
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
@@ -54,27 +63,39 @@ public class OrderDetailsActivity extends AppCompatActivity {
         txt_delivery_address = findViewById(R.id.txt_delivery_address);
         txt_order_notes = findViewById(R.id.txt_order_notes);
         btn_next_state = findViewById(R.id.btn_next_state);
+        card_rider = findViewById(R.id.card_rider);
+        txt_rider_name = findViewById(R.id.txt_rider_name);
+        txt_rider_phone = findViewById(R.id.txt_rider_phone);
     }
 
-    private void feedViews(Order selctedOrder) {
-        txt_order_number.setText("" + selctedOrder.getNumber());
-        txt_delivery_time.setText(selctedOrder.getDelivery_time());
-        txt_delivery_date.setText(selctedOrder.getDelivery_date());
-        txt_customer_name.setText(selctedOrder.getCustomer_name());
-        txt_customer_phone.setText(Html.fromHtml("<u>" + selctedOrder.getCustomer_phone() + "<u/>"));
-        txt_delivery_address.setText(selctedOrder.getDelivery_address());
-        txt_order_notes.setText(selctedOrder.getNotes());
+    private void feedViews(Order selectedOrder) {
+        txt_order_number.setText("" + selectedOrder.getId());
+        txt_delivery_time.setText(selectedOrder.getDelivery_timestamp().split(" ")[1]);
+        txt_delivery_date.setText(selectedOrder.getDelivery_timestamp().split(" ")[0]);
+        txt_customer_name.setText(selectedOrder.getCustomer_name());
+        txt_customer_phone.setText(Html.fromHtml("<u>" + selectedOrder.getCustomer_phone() + "<u/>"));
+        txt_delivery_address.setText(selectedOrder.getDelivery_address());
+        txt_order_notes.setText(selectedOrder.getNotes());
 
         String order_content = "";
-        for (String item : selctedOrder.getItem_itemQuantity().keySet()) {
+        for (String item : selectedOrder.getItem_itemQuantity().keySet()) {
             if (!order_content.equals(""))
-                order_content += System.lineSeparator();
-            order_content += "x" + selctedOrder.getItem_itemQuantity().get(item) + " " + item;
+                order_content += "\n";
+            order_content += "x" + selectedOrder.getItem_itemQuantity().get(item) + " " + item;
         }
         txt_order_content.setText(order_content);
 
-        txt_state_history.setText(selctedOrder.getStateHistoryToString());
 
+        if (!TextUtils.isEmpty(selectedOrder.getRider_id())) {
+            txt_rider_name.setText(selectedOrder.getRider_name());
+            txt_rider_phone.setText(Html.fromHtml("<u>" + selectedOrder.getRider_phone() + "<u/>"));
+            txt_rider_phone.setOnClickListener(v -> {
+                String phoneNumber = ((TextView) v).getText().toString();
+                startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null)));
+            });
+        }
+
+        txt_state_history.setText(selectedOrder.getStateHistoryToString());
     }
 
     private void showConfirmationDialog() {
@@ -99,39 +120,66 @@ public class OrderDetailsActivity extends AppCompatActivity {
     }
 
     private void positiveButtonAction() {
-        if (inputOrders.get(inputPosition).moveToNextState()) {
-            Intent intent = new Intent();
+        if (inputOrder.moveToNextState()) {
 
-            ArrayList<Order> updatedOrders = new ArrayList<Order>();
-            updatedOrders.addAll(inputOrders);
+            txt_state_history.setText(inputOrder.getStateHistoryToString());
 
-            Collections.sort(updatedOrders, new Comparator<Order>() {
-                @Override
-                public int compare(Order o1, Order o2) {
-                    if (o1.getCurrentState() == Order.STATE3)
-                        return 1;
-                    else
-                        return -o1.getDelivery_timestamp().compareTo(o2.getDelivery_timestamp());
-                }
-            });
-
-            intent.putExtra("orders", updatedOrders);
-            intent.putExtra("position", inputPosition);
-
-            setResult(STATE_CHANGED, intent);
-            txt_state_history.setText(inputOrders.get(inputPosition).getStateHistoryToString());
-
-        } else
-            setResult(STATE_NOT_CHANGED);
-
-        if (inputOrders.get(inputPosition).getCurrentState() == Order.STATE3) {
-            btn_next_state.setTextColor(getColor(R.color.button_disabled_text));
-            btn_next_state.setEnabled(false);
+            if (inputOrder.getCurrentState() == Order.STATE3) {
+                btn_next_state.setTextColor(getResources().getColor(R.color.button_disabled_text));
+                btn_next_state.setEnabled(false);
+                selectRider();
+                card_rider.setVisibility(View.VISIBLE);
+            }
+            FirebaseDatabase.getInstance()
+                    .getReference("/restaurateurs/" + FirebaseAuth.getInstance().getUid() + "/orders/" + inputOrder.getId())
+                    .setValue(inputOrder);
         }
+
     }
 
     private void negativeButtonAction() {
         setResult(STATE_NOT_CHANGED);
+    }
+
+    private void selectRider() {
+
+        //choose rider from firebase
+        String rider_id = "trial_rider_id";
+        String rider_name = "trial_rider_name";
+        String rider_phone = "3319898789";
+
+        //update the order
+        inputOrder.setRider_id(rider_id);
+        inputOrder.setRider_name(rider_name);
+        inputOrder.setRider_phone(rider_phone);
+        FirebaseDatabase.getInstance().getReference().child("orders").child(inputOrder.getId()).setValue(inputOrder);
+
+        //notify rider
+
+        txt_rider_name.setText(inputOrder.getRider_name());
+        txt_rider_phone.setText(Html.fromHtml("<u>" + inputOrder.getRider_phone() + "<u/>"));
+        txt_rider_phone.setOnClickListener(v -> {
+            String phoneNumber = ((TextView) v).getText().toString();
+            startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null)));
+        });
+    }
+
+    public void adjustLayoutProgrammatically() {
+        if (inputOrder.getCurrentState() != Order.STATE3) {
+            btn_next_state.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    showConfirmationDialog();
+                }
+            });
+            card_rider.setVisibility(View.GONE);
+        } else {
+            card_rider.setVisibility(View.VISIBLE);
+            btn_next_state.setTextColor(getResources().getColor(R.color.button_disabled_text));
+            btn_next_state.setEnabled(false);
+        }
+
+        feedViews(inputOrder);
     }
 
     @Override
@@ -143,30 +191,33 @@ public class OrderDetailsActivity extends AppCompatActivity {
 
         locateViews();
 
-        txt_customer_phone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String phoneNumber = ((TextView) v).getText().toString();
-                startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null)));
-            }
+        txt_customer_phone.setOnClickListener(v -> {
+            String phoneNumber = ((TextView) v).getText().toString();
+            startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null)));
         });
 
-        inputOrders = (ArrayList<Order>) getIntent().getExtras().getBundle("args").getSerializable("orders");
-        inputPosition = getIntent().getExtras().getBundle("args").getInt("position");
+        if (!TextUtils.isEmpty(getIntent().getStringExtra("id"))) {
+            // we came here due to a tap on the notification so let us read the (updated) order from firebase
+            FirebaseDatabase.getInstance().getReference()
+                    .child("orders")
+                    .child(getIntent().getStringExtra("id"))
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            inputOrder = dataSnapshot.getValue(Order.class);
+                            adjustLayoutProgrammatically();
+                        }
 
-        if (inputOrders.get(inputPosition).getCurrentState() != Order.STATE3)
-            btn_next_state.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    showConfirmationDialog();
-                }
-            });
-        else {
-            btn_next_state.setTextColor(getColor(R.color.button_disabled_text));
-            btn_next_state.setEnabled(false);
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+        } else {
+            // we came here tapping on an order in the recycler view
+            inputOrder = (Order) getIntent().getSerializableExtra("order");
+            adjustLayoutProgrammatically();
         }
-
-        feedViews(inputOrders.get(inputPosition));
     }
 
     @Override
