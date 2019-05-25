a873ad25d38c1324ba9a175f47893576d84be93d
Pdipedro25 <34885983+Pdipedro25@users.noreply.github.com>
2019-05-25 17:41:36 +0200

    Statistics fragment modified and added new stats

--- deliveryman/src/main/java/it/polito/mad1819/group17/deliveryapp/deliveryman/statistics/StatsFragment.java
+++ deliveryman/src/main/java/it/polito/mad1819/group17/deliveryapp/deliveryman/statistics/StatsFragment.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -42,13 +44,23 @@ public class StatsFragment extends Fragment {
     private TextView km_deliveredTv;
     private TextView km_assignedTv;
     private TextView counter;
+
+    private TextView counter_red;
+    private TextView counter_yellow;
+    private TextView counter_green;
+
     private TextView numberOfCals;
     private ProgressBar pieChart;
 
     private ProgressBarHandler progressBarHandler;
+    private SwipeRefreshLayout pullToRefresh;
+
 
     private DecimalFormat df;
     int count;
+    int count_assigned;
+    int count_delivered;
+    int count_accepted;
 
     // TODO: Rename and change types of parameters
 
@@ -77,15 +89,6 @@ public class StatsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        km_accepted =0;
-        km_delivered = 0;
-        km_assigned = 0;
-        count = 0;
-        df = new DecimalFormat("#.###");
-
-        retrieveKms();
-
     }
 
     public void onStart() {
@@ -106,12 +109,38 @@ public class StatsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_stats, container, false);
         progressBarHandler = new ProgressBarHandler(getContext());
+
+        km_accepted =0;
+        km_delivered = 0;
+        km_assigned = 0;
+        count = 0;
+        int count_assigned=0;
+        int count_delivered=0;
+        int count_accepted=0;
+        df = new DecimalFormat("#.###");
+
         locateViews(view);
+
+        retrieveKms(false);
 
         return view;
 
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        pullToRefresh = (SwipeRefreshLayout) view.findViewById(R.id.swipe_layout);
+        pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                updateStats();
+                updateChart();
+                pullToRefresh.setRefreshing(false);
+            }
+        });
+    }
 
     public void locateViews(View view){
         km_acceptedTv = view.findViewById(R.id.kmAccepted);
@@ -121,15 +150,12 @@ public class StatsFragment extends Fragment {
         numberOfCals = view.findViewById(R.id.number_of_km);
         pieChart = view.findViewById(R.id.stats_progressbar);
 
-
+        counter_green = view.findViewById(R.id.requestCounterGreen);
+        counter_yellow = view.findViewById(R.id.requestCounterYellow);
+        counter_red = view.findViewById(R.id.requestCounterRed);
     }
 
-    public void retrieveKms(){
-
-        km_accepted =0;
-        km_delivered = 0;
-        km_assigned = 0;
-        count = 0;
+    public void retrieveKms(boolean isRefreshing){
 
         DatabaseReference reference = FirebaseDatabase.getInstance()
                 .getReference()
@@ -138,9 +164,15 @@ public class StatsFragment extends Fragment {
                 .child("delivery_requests");
 
 
-        reference.addValueEventListener(new ValueEventListener() {
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                /*km_accepted =0;
+                km_delivered = 0;
+                km_assigned = 0;
+                count = 0;*/
+
                 for(DataSnapshot ds : dataSnapshot.getChildren()) {
 
                     count += 1;
@@ -154,24 +186,26 @@ public class StatsFragment extends Fragment {
                         case DeliveryRequest.STATE1:
                             //ASSIGNED (red)
                             km_assigned +=  Double.valueOf(ds.child("distance").getValue(String.class))/1000;//FROM METERS TO KILOMETERS
-                            Log.d("VALUESSS","assigned,"+km_assigned);
+                            count_assigned+=1;
 
                             break;
                         case DeliveryRequest.STATE2:
                             //ACCEPTED (yellow)
                             km_accepted +=  Double.valueOf(ds.child("distance").getValue(String.class))/1000;//FROM METERS TO KILOMETERS
-                            Log.d("VALUESSS","accepted"+km_accepted);
+                            count_accepted +=1;
 
                             break;
                         case DeliveryRequest.STATE3:
                             //DELIVERED (green)
                             km_delivered +=  Double.valueOf(ds.child("distance").getValue(String.class))/1000;//FROM METERS TO KILOMETERS
-                            Log.d("VALUESSS","delivered"+km_delivered);
+                            count_delivered +=1;
                             break;
                     }
                 }
                 pb_accepted = km_accepted / (km_delivered+km_assigned+km_accepted);
 
+                updateStats();
+                updateChart();
             }
 
             @Override
@@ -179,7 +213,7 @@ public class StatsFragment extends Fragment {
 
             }
         });
-
+        if(isRefreshing) pullToRefresh.setRefreshing(false);
     }
 
 
@@ -197,14 +231,18 @@ public class StatsFragment extends Fragment {
         km_assignedTv.setText(df.format(km_assigned)+" km");
         counter.setText(""+count);
 
+        counter_green.setText(""+count_delivered);
+        counter_yellow.setText(""+count_accepted);
+        counter_red.setText(""+count_assigned);
+
     }
 
     public void updateChart(){
         double base = km_delivered+km_assigned+km_accepted;
         pb_accepted = km_delivered / base;
-        Log.d("double_kms",""+km_accepted+","+km_assigned+","+km_delivered);
-        Log.d("double_base",""+base);
-        Log.d("double_d",""+ pb_accepted);
+        //Log.d("double_kms",""+km_accepted+","+km_assigned+","+km_delivered);
+        //Log.d("double_base",""+base);
+        //Log.d("double_d",""+ pb_accepted);
         int progress = 0;
         progress = (int) (pb_accepted * 100);
         Log.d("progress",""+progress);
