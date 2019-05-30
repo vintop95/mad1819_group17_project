cd04dd196b0c5916d4f489ad7365266bafb4f8f2
valeriomieuli <valeriomieuli@gmail.com>
2019-05-30 15:58:44 +0200

    In deliveryman's app: bug fix about sorting delivery requests.

---
+++ deliveryman/src/main/java/it/polito/mad1819/group17/deliveryapp/deliveryman/AnalyticsActivity.java
@@ -0,0 +1,13 @@
+package it.polito.mad1819.group17.deliveryapp.deliveryman;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnalyticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analytics);
+    }
+}
